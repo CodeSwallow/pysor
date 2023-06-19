909e312133ed9554859f38636cc45075c14f84ed
Isai Ramirez <86127102+CodeSwallow@users.noreply.github.com>
2023-06-19 14:14:30 -0600

    feat: created mqtt client class

---
+++ sensor_simulation/mqtt_client.py
@@ -0,0 +1,31 @@
+import paho.mqtt.client as mqtt
+
+
+class MqttClient:
+    """
+    A class used to represent a MQTT client
+    """
+
+    def __init__(self, broker_address: str) -> None:
+        self.broker_address = broker_address
+        self.client = mqtt.Client()
+        self.client.connect(self.broker_address)
+
+    def publish(self, topic: str, message: str) -> None:
+        """
+        Publish a message to a topic
+
+        :param topic: Name of the topic
+        :param message: Message to be published
+        :return: None
+        """
+        self.client.publish(topic, message)
+
+    def subscribe(self, topic: str) -> None:
+        """
+        Subscribe to a topic
+
+        :param topic: Name of the topic
+        :return: None
+        """
+        self.client.subscribe(topic)
