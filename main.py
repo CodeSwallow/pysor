7361e75a15ef7bc59196a927bd8aae500c910edc
Isai Ramirez <86127102+CodeSwallow@users.noreply.github.com>
2023-06-19 15:45:33 -0600

    test: testing temperature sensor class

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from sensor_simulation import MqttClient
+from sensor_simulation.sensors import TemperatureSensor
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    mqtt_client = MqttClient("test.mosquitto.org")
+    temperature_sensor = TemperatureSensor(mqtt_client, "temperature", 1, 10, 30)
+    temperature_sensor.start_publishing()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
